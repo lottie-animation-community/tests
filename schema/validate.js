e51a23ce68d57b0e1e2ea803f9825ffe83508991
airnan <hernantorrisi@gmail.com>
2021-10-13 07:40:35 -0400

    added error alerts

--- schema/validate.js
+++ schema/validate.js
@@ -1,21 +1,49 @@
 const fs = require('fs');
+const core = require('@actions/core');
+const github = require('@actions/github');
 const validate = require('./index.js');
 
 const examplesDirectory = '../examples/'
+let totalFiles = 0;
+let failedFiles = [];
+
+function validateFile(error, data, fileName) {
+  try {
+    if (error) {
+      throw error;
+    } else {
+      const schemaValidation = validate(JSON.parse(data));
+      if (schemaValidation?.errors.length) {
+        failedFiles.push({
+          fileName,
+          error: schemaValidation.errors,
+        })
+      } else {
+      }
+    }
+  } catch (error) {
+    failedFiles.push({
+      fileName,
+      error,
+    })
+  }
+  fileProcessed()
+}
+
+function fileProcessed() {
+  totalFiles -= 1;
+  if (totalFiles === 0) {
+    if (failedFiles.length) {
+      core.setFailed(failedFiles);
+    } else {
+      console.log('NO ERRORS');
+    }
+  }
+}
 
 fs.readdir(examplesDirectory, (err, files) => {
-  files.forEach(file => {
-      fs.readFile(examplesDirectory + file,  "utf8",  function (error, data) {
-        try {
-          const schemaValidation = validate(JSON.parse(data));
-          if (schemaValidation?.errors.length) {
-            console.log('HAS ERRORS')
-          } else {
-            console.log('NO ERRORS')
-          }
-        } catch (error) {
-          console.log('ERROR WHILE VALIDATING')
-        }
-      });
+  totalFiles = files.length;
+  files.forEach(fileName => {
+      fs.readFile(examplesDirectory + fileName,  'utf8',  (error, data) => validateFile(error, data, fileName));
   });
 });
