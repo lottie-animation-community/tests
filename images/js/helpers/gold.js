c5498f4906600b38583eb8eb9f04c91e72cb2057
airnan <hernantorrisi@gmail.com>
2022-02-08 18:15:20 -0500

    added comments to gold methods

--- images/js/helpers/gold.js
+++ images/js/helpers/gold.js
@@ -1,6 +1,7 @@
 const execToPromise = require('../utils/execToPromise');
 const writeToPromise = require('../utils/writeToPromise');
 
+// Takes the secret from the env variable and creates the secret.json file needed to auth users
 const writeSecret = async () => {
   try {
     const googleEnvSecret = process.env.GOOGLE_CLOUD_STORAGE;
@@ -12,6 +13,8 @@ const writeSecret = async () => {
   }
 };
 
+// Creates the keys.json file with the metadata associated to the images
+// TODO: decide what additional information would be good to include
 const createKeysFile = async () => {
   const keys = {
     origin: 'github',
@@ -21,14 +24,18 @@ const createKeysFile = async () => {
 
 const initialize = async () => {
   await writeSecret();
+  // authentication process
   await execToPromise('goldctl auth --work-dir ./tmp --service-account ./secret.json');
-  const githubCommit = process.env.GITHUB_SHA;
   await createKeysFile();
+  const githubCommit = process.env.GITHUB_SHA;
+  // initalizes the process
   await execToPromise(`goldctl imgtest init --work-dir ./tmp --commit ${githubCommit} --keys-file ./keys.json --instance lottie-animation-community --bucket lottie-animation-community-tests`);
 };
 
 const uploadImage = async (imagePath, testName) => {
   try {
+    // Adds an image to the current imgtest process.
+    // the --test-name argument should be different for each animation
     await execToPromise(`goldctl imgtest add --work-dir ./tmp --test-name "${testName}" --png-file "${imagePath}"`);
   } catch (error) {
     //
@@ -37,6 +44,7 @@ const uploadImage = async (imagePath, testName) => {
 
 const finalize = async () => {
   try {
+    // finalizes the process
     await execToPromise('goldctl imgtest finalize --work-dir ./tmp');
   } catch (error) {
     //
